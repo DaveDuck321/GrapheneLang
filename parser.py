b7800c2a5d34ea4bd052757c5cafe756ea1abe5d
Antros Economides <antroseconomides@hotmail.co.uk>
2022-11-10 20:01:46 +0000

    Remove unnecessary "start" rule

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from lark import Lark
 
 with open("grammar.lark") as grammar:
-    l = Lark(grammar)
+    l = Lark(grammar, start="program")
 
 print(l.parse("""typedef foo: (int, int) -> int;""").pretty())
