1f2f7d95eefee0eac57fb8591b0ba3499db1d24b
Tom Grant <thomas.grant.duck321@gmail.com>
2022-11-10 23:18:42 +0000

    Simple test harness with JSON config

---
+++ tests/run_tests.py
@@ -0,0 +1,135 @@
+import json
+import schema
+import subprocess
+
+from argparse import ArgumentParser
+from pathlib import Path
+from subprocess import PIPE
+
+all_tests = ["c_demo/runtime_status", "c_demo/compile_error"]
+
+
+class TestFailure(RuntimeError):
+    def __init__(self, name, status, stdout, stderr, stage=None):
+        super().__init__(f"Actual '{name}' does not match expected")
+
+        self.name = name
+        self.status = status
+        self.stdout = stdout
+        self.stderr = stderr
+        self.stage = stage
+
+    def __str__(self) -> str:
+        return "\n".join(
+            [
+                f"***{self.stage.upper()} ERROR: Actual '{self.name}' does not match expected",
+                "====status====",
+                str(self.status),
+                "====stdout====",
+                *self.stdout,
+                "====stderr====",
+                *self.stderr,
+            ]
+        )
+
+
+def validate_command_status(directory: Path, command: list[str], expected_output):
+    status = subprocess.call(command, cwd=str(directory))
+
+    if expected_output.get("status", 0) != status:
+        print("*** ERROR: Actual 'status' does not match expected")
+
+
+def validate_command_output_with_harness(
+    directory: Path,
+    command: list[str],
+    expected_output,
+):
+
+    result = subprocess.run(
+        command, cwd=str(directory), stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True
+    )
+    status = result.returncode
+    stdout, stderr = result.stdout.splitlines(), result.stderr.splitlines()
+
+    if expected_output.get("status", 0) != status:
+        raise TestFailure("status", status, stdout, stderr)
+
+    if expected_output.get("stdout", stdout) != stdout:
+        raise TestFailure(f"stdout", status, stdout, stderr)
+
+    if expected_output.get("stderr", stderr) != stderr:
+        raise TestFailure(f"stderr", status, stdout, stderr)
+
+    return True
+
+
+def run_test(path: Path, io_harness=True):
+    # Load/ validate the test
+    config_path = path / "test.json"
+    assert config_path.exists()
+
+    (path / "out").mkdir(exist_ok=True)
+
+    config = json.load(config_path.open())
+    schema.validate_config_follows_schema(config)
+
+    fn_validate = (
+        validate_command_output_with_harness if io_harness else validate_command_status
+    )
+
+    # Run the test
+    try:
+        fn_validate(
+            path,
+            config["compile"].get("command", []),
+            config["compile"].get("output", {}),
+        )
+    except TestFailure as e:
+        raise TestFailure(e.name, e.status, e.stdout, e.stderr, stage="compile")
+
+    if "runtime" not in config:
+        return
+
+    try:
+        fn_validate(
+            path,
+            config["runtime"].get("command", []),
+            config["runtime"].get("output", {}),
+        )
+    except TestFailure as e:
+        raise TestFailure(e.name, e.status, e.stdout, e.stderr, stage="runtime")
+
+
+def run_tests(tests):
+    passed = []
+    failed = []
+
+    for i, test in enumerate(tests):
+        print(f"TEST {i+1}: '{test}'")
+        try:
+            run_test(Path(__file__).parent / test)
+            passed.append(test)
+            print("   PASSED")
+        except TestFailure as error:
+            failed.append(test)
+            print(error)
+            print()
+
+    if len(failed) == 0:
+        print(f"PASSED {len(passed)} TESTS")
+        return 0
+
+    print(f"FAILED {len(failed)}/{len(tests)} TESTS!!")
+    return len(failed)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("run_tests.py")
+    parser.add_argument("--test", required=False)
+
+    args = parser.parse_args()
+    if args.test is not None:
+        run_test(Path(__file__).parent / args.test, io_harness=False)
+    else:
+        exit(run_tests(all_tests))
---
+++ tests/schema.py
@@ -0,0 +1,39 @@
+io_schema = {"status": 0, "stdout": [""], "stderr": [""]}
+config_schema = {
+    "compile": {"command": [""], "output": io_schema},
+    "runtime": {"command": [""], "output": io_schema},
+}
+
+
+class JSONConfigError(ValueError):
+    pass
+
+
+def validate_config_follows_schema(config: dict, current_key="", schema=config_schema):
+    for key, option in config.items():
+        full_key = f"{current_key}.{key}"
+
+        # Validate keys
+        if key not in schema:
+            raise JSONConfigError(f"'{full_key}' not recognized")
+
+        # Validate types
+        schema_value = schema[key]
+        if type(option) != type(schema_value):
+            raise JSONConfigError(
+                f"'{full_key}' must be of type '{type(schema_value)}' not '{type(option)}'"
+            )
+
+        # Validate list elements are also the correct type
+        if isinstance(schema_value, list):
+            for item in option:
+                if type(item) != type(schema_value[0]):
+                    raise JSONConfigError(
+                        f"'{full_key}' must be of type '[{type(schema_value[0])}]' not '[{type(item)}]'"
+                    )
+
+        # Recurse for dict types
+        if isinstance(schema_value, dict):
+            validate_config_follows_schema(option, full_key, schema_value)
+
+    return True
