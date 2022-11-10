c0165d92fa2b7291a0d8153a4670b8e32afcb633
Antros Economides <antroseconomides@hotmail.co.uk>
2022-11-10 20:13:01 +0000

    Parse from file

--- parser.py
+++ parser.py
@@ -3,4 +3,7 @@ from lark import Lark
 with open("grammar.lark") as grammar:
     l = Lark(grammar, parser="lalr", start="program")
 
-print(l.parse("""typedef foo: (int, int) -> int;""").pretty())
+with open("demo.c3") as source:
+    tree = l.parse(source.read())
+
+    print(tree.pretty())
