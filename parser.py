072ff93c92ffabee92d8ceb1adeef3f239212e73
Antros Economides <antroseconomides@hotmail.co.uk>
2022-11-10 20:00:34 +0000

    Pretty-print parsed tree

--- parser.py
+++ parser.py
@@ -3,4 +3,4 @@ from lark import Lark
 with open("grammar.lark") as grammar:
     l = Lark(grammar)
 
-print(l.parse("""typedef foo: (int, int) -> int;"""))
+print(l.parse("""typedef foo: (int, int) -> int;""").pretty())
