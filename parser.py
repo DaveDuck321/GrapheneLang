3290c92450cae1a9a5e0870f453269296611e196
Antros Economides <antroseconomides@hotmail.co.uk>
2022-11-10 20:52:06 +0000

    Let Lark open the grammer file

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 from lark import Lark
 
-with open("grammar.lark") as grammar:
-    l = Lark(grammar, parser="lalr", start="program")
+l = Lark.open("grammar.lark", parser="lalr", start="program")
 
 with open("demo.c3") as source:
     tree = l.parse(source.read())
