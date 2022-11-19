eb406487b69ee2f5e23649cb61fcd268ca764545
Tom Grant <thomas.grant.mail@gmail.com>
2022-11-19 06:02:11 +0000

    Parser: rename codegen -> cg

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from lark import Lark, Token, Tree
 from lark.visitors import Interpreter, Transformer_InPlace
 
-import codegen
+import codegen as cg
 
 
 def extract_leaf_value(tree: Tree) -> str:
@@ -30,11 +30,11 @@ def extract_named_leaf_value(tree: Tree, name: str) -> str:
 
 class SymbolTableGenerator(Interpreter):
     # TODO: also parse typedefs
-    def __init__(self, program: codegen.Program) -> None:
+    def __init__(self, program: cg.Program) -> None:
         super().__init__()
 
         self._program = program
-        self._function_body_trees: list[tuple[codegen.Function, Tree]] = []
+        self._function_body_trees: list[tuple[cg.Function, Tree]] = []
 
     def get_function_body_trees(self):
         return self._function_body_trees
@@ -50,9 +50,9 @@ class SymbolTableGenerator(Interpreter):
             type_name = extract_named_leaf_value(arg_type, "type_name")
 
             type = self._program.lookup_type(type_name)
-            fn_args.append(codegen.Variable(name, type))
+            fn_args.append(cg.Variable(name, type))
 
-        fn_signature = codegen.FunctionSignature(fn_name, fn_args)
+        fn_signature = cg.FunctionSignature(fn_name, fn_args)
 
         # TODO parse adhoc/ generic types.
         fn_return_type_tree = get_unique_child(tree, "function_return_type")
@@ -61,7 +61,7 @@ class SymbolTableGenerator(Interpreter):
         fn_return_type = self._program.lookup_type(fn_return_type_name)
 
         # Build the function
-        function_obj = codegen.Function(fn_signature, fn_return_type)
+        function_obj = cg.Function(fn_signature, fn_return_type)
         self._program.add_function(function_obj)
 
         # Save the body to parse later (TODO: maybe forward declarations should be possible?)
@@ -71,29 +71,29 @@ class SymbolTableGenerator(Interpreter):
 
 @dataclass
 class FlattenedExpression:
-    subexpressions: list[codegen.TypedExpression]
+    subexpressions: list[cg.TypedExpression]
 
-    def add_parent(self, expression: codegen.TypedExpression) -> "FlattenedExpression":
+    def add_parent(self, expression: cg.TypedExpression) -> "FlattenedExpression":
         self.subexpressions.append(expression)
         return self
 
-    def expression(self) -> codegen.TypedExpression:
+    def expression(self) -> cg.TypedExpression:
         return self.subexpressions[-1]
 
-    def type(self) -> codegen.TypedExpression:
+    def type(self) -> cg.TypedExpression:
         return self.expression().type
 
 
 class ExpressionTransformer(Transformer_InPlace):
-    def __init__(self, program: codegen.Program, function: codegen.Function) -> None:
+    def __init__(self, program: cg.Program, function: cg.Function) -> None:
         super().__init__(visit_tokens=True)
 
         self._program = program
         self._function = function
 
     def SIGNED_INT(self, value: str) -> FlattenedExpression:
-        const_expr = codegen.ConstantExpression(
-            self._function.get_next_expr_id(), codegen.IntType(), int(value)
+        const_expr = cg.ConstantExpression(
+            self._function.get_next_expr_id(), cg.IntType(), int(value)
         )
         return FlattenedExpression([const_expr])
 
@@ -106,16 +106,16 @@ class ExpressionTransformer(Transformer_InPlace):
         for arg in children[1].children:
             assert isinstance(arg, FlattenedExpression)
             # FIXME name?
-            var = codegen.Variable("", arg.type())
+            var = cg.Variable("", arg.type())
             args_for_signature.append(var)
             fn_args.append(arg.expression())
 
             flattened_expr.subexpressions.extend(arg.subexpressions)
 
-        fn_signature = codegen.FunctionSignature(fn_name, args_for_signature)
+        fn_signature = cg.FunctionSignature(fn_name, args_for_signature)
         fn = self._program.lookup_function(fn_signature)
 
-        call_expr = codegen.FunctionCallExpression(
+        call_expr = cg.FunctionCallExpression(
             self._function.get_next_expr_id(), fn, fn_args)
 
         return flattened_expr.add_parent(call_expr)
@@ -124,13 +124,13 @@ class ExpressionTransformer(Transformer_InPlace):
         assert string[0] == '"' and string[-1] == '"'
         identifier = self._program.add_string(string[1:-1])
 
-        str_const = codegen.StringConstant(
+        str_const = cg.StringConstant(
             self._function.get_next_expr_id(), identifier)
 
         return FlattenedExpression([str_const])
 
 
-def generate_standalone_expression(program: codegen.Program, function: codegen.Function, body: Tree) -> None:
+def generate_standalone_expression(program: cg.Program, function: cg.Function, body: Tree) -> None:
     assert len(body.children) == 1
     ExpressionTransformer(program, function).transform(body)
 
@@ -141,9 +141,9 @@ def generate_standalone_expression(program: codegen.Program, function: codegen.F
         function.expressions.append(subexpression)
 
 
-def generate_return_statement(program: codegen.Program, function: codegen.Function, body: Tree) -> None:
+def generate_return_statement(program: cg.Program, function: cg.Function, body: Tree) -> None:
     if not body.children:
-        expr = codegen.ReturnExpression(function.get_next_expr_id())
+        expr = cg.ReturnExpression(function.get_next_expr_id())
         function.expressions.append(expr)
         return
 
@@ -156,13 +156,13 @@ def generate_return_statement(program: codegen.Program, function: codegen.Functi
     for subexpression in flattened_expr.subexpressions:
         function.expressions.append(subexpression)
 
-    expr = codegen.ReturnExpression(
+    expr = cg.ReturnExpression(
         function.get_next_expr_id(), flattened_expr.expression())
 
     function.expressions.append(expr)
 
 
-def generate_function_body(program: codegen.Program, function: codegen.Function, body: Tree):
+def generate_function_body(program: cg.Program, function: cg.Function, body: Tree):
     assert body.data == "scope"
 
     generators = {"return_statement": generate_return_statement,
@@ -178,14 +178,14 @@ with open("demo.c3") as source:
 
     print(tree.pretty())
 
-    program = codegen.Program()
+    program = cg.Program()
     symbol_table_gen = SymbolTableGenerator(program)
     symbol_table_gen.visit(tree)
 
     program.add_function(
-        codegen.Function(
-            codegen.FunctionSignature(
-                "puts", [codegen.Variable(
+        cg.Function(
+            cg.FunctionSignature(
+                "puts", [cg.Variable(
                     "string", program.lookup_type("string"))]
             ),
             program.lookup_type("int"),
