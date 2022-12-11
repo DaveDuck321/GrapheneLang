c8973a5fd003594ce0b975704f1572a34cc5b228
Antros Economides <antroseconomides@hotmail.co.uk>
2022-12-11 22:21:40 +0200

    codegen: sort imports

--- codegen/__init__.py
+++ codegen/__init__.py
@@ -1,13 +1,3 @@
-from .translation_unit import Program, Function, Scope
-from .interfaces import Variable, TypedExpression, Type, Parameter
-from .generatable import VariableAssignment, ReturnStatement, IfStatement
-from .expressions import (
-    ConstantExpression,
-    StackVariable,
-    StringConstant,
-    StructMemberAccess,
-    VariableAccess,
-)
 from .builtin_types import (
     BoolType,
     IntType,
@@ -15,3 +5,13 @@ from .builtin_types import (
     StringType,
     StructDefinition,
 )
+from .expressions import (
+    ConstantExpression,
+    StackVariable,
+    StringConstant,
+    StructMemberAccess,
+    VariableReference,
+)
+from .generatable import IfStatement, ReturnStatement, VariableAssignment
+from .interfaces import Parameter, Type, TypedExpression, Variable
+from .translation_unit import Function, Program, Scope
