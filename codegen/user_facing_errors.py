d2d8d1d4895a899080ac7433fde4012e1f268496
Antros Economides <antroseconomides@hotmail.co.uk>
2022-12-16 12:37:29 +0200

    GenericArgumentCountError: use consistent quotes
    All other exceptions use ' instead of `.

--- codegen/user_facing_errors.py
+++ codegen/user_facing_errors.py
@@ -77,7 +77,7 @@ class InvalidIntSize(GrapheneError):
 class GenericArgumentCountError(GrapheneError):
     def __init__(self, name: str, actual: int, expected: int) -> None:
         super().__init__(
-            f"Error: generic `{name}` expects {expected} arguments but received {actual}"
+            f"Error: generic '{name}' expects {expected} arguments but received {actual}"
         )
 
 
