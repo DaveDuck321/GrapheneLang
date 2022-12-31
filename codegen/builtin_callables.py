bfd3fd98e431ab885e4847a7f1de777e0212a135
Antros Economides <antroseconomides@hotmail.co.uk>
2022-12-31 15:52:37 +0200

    builtin_callables: remove unused import

--- codegen/builtin_callables.py
+++ codegen/builtin_callables.py
@@ -4,7 +4,7 @@ from typing import Callable, Iterator
 from .builtin_types import GenericIntType, IntegerDefinition
 from .interfaces import TypedExpression
 from .type_conversions import do_implicit_conversion
-from .user_facing_errors import TypeCheckerError, OperandError, throw
+from .user_facing_errors import OperandError, throw
 
 
 class AddExpression(TypedExpression):
