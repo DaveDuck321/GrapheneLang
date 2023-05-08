DaveDuck321/GrapheneLang

commit 51beb340d05d7b7d4dc83452ae0b1ff288fb8168

IfElseStatement: don't generate end label if both branches return
Fixes #72

codegen/generatable.py
@@ -195,26 +195,41 @@ class IfElseStatement(Generatable):
 
     def generate_ir(self, reg_gen: Iterator[int]) -> list[str]:
         # https://llvm.org/docs/LangRef.html#br-instruction
+        need_label_after_statement = False
 
         conv_condition, extra_exprs = do_implicit_conversion(self.condition, BoolType())
 
+        ir = self.expand_ir(extra_exprs, reg_gen)
+
         # br i1 <cond>, label <iftrue>, label <iffalse>
-        return [
-            *self.expand_ir(extra_exprs, reg_gen),
-            (
-                f"br {conv_condition.ir_ref_with_type_annotation}, label "
-                f"%{self.if_scope.start_label}, label %{self.else_scope.start_label}"
-            ),
-            # If body
-            f"{self.if_scope.start_label}:",
-            *self.if_scope.generate_ir(reg_gen),
-            f"br label %{self.else_scope.end_label}",
-            # Else body
-            f"{self.else_scope.start_label}:",
-            *self.else_scope.generate_ir(reg_gen),
-            f"br label %{self.else_scope.end_label}",
-            f"{self.else_scope.end_label}:",
-        ]
+        ir.append(
+            f"br {conv_condition.ir_ref_with_type_annotation}, "
+            f"label %{self.if_scope.start_label}, label %{self.else_scope.start_label}"
+        )
+
+        # If body
+        ir.append(f"{self.if_scope.start_label}:")
+        ir.extend(self.if_scope.generate_ir(reg_gen))
+
+        if not self.if_scope.is_return_guaranteed():
+            # Jump to the end of the if/else statement.
+            ir.append(f"br label %{self.else_scope.end_label}")
+            need_label_after_statement = True
+
+        # Else body
+        ir.append(f"{self.else_scope.start_label}:")
+        ir.extend(self.else_scope.generate_ir(reg_gen))
+
+        if not self.else_scope.is_return_guaranteed():
+            # Jump to the end of the if/else statement.
+            ir.append(f"br label %{self.else_scope.end_label}")
+            need_label_after_statement = True
+
+        if need_label_after_statement:
+            # LLVM will complain if this is empty.
+            ir.append(f"{self.else_scope.end_label}:")
+
+        return ir
 
     def is_return_guaranteed(self) -> bool:
         return (
@@ -306,9 +321,6 @@ class ReturnStatement(Generatable):
 
         # ret <type> <value>; Return a value from a non-void function
         ir_lines.append(f"ret {conv_returned_expr.ir_ref_with_type_annotation}")
-
-        # An implicit basic block is created immediately after a return
-        next(reg_gen)
 
         return ir_lines
 

tests/regressions/return_from_both_if_branches.c3
@@ -1,0 +1,15 @@
+function foo : () -> void = {
+    if (true) {
+        return;
+    } else {
+        return;
+    }
+};
+
+function main : () -> int = {
+    foo();
+    return 0;
+};
+
+/// @COMPILE
+/// @RUN

commit 472a3e58dd2e402642e95e77f00e72257f0f4e79

Don't generate instructions after guaranteed return
Fixes a small regression in: 51beb340d05d7b7d4dc83452ae0b1ff288fb8168

LLVM complains if we generate unreachable code without a preceding
label, this commit guarantees that unreachable code is not passed to the
frontend.

codegen/generatable.py
@@ -158,11 +158,13 @@ class Scope(Generatable):
         for variable in self._variables.values():
             contained_ir.extend(variable.generate_ir(reg_gen))
 
-        for lines in self._lines:
-            contained_ir.extend(lines.generate_ir(reg_gen))
+        for line in self._lines:
+            contained_ir.extend(line.generate_ir(reg_gen))
+            if line.is_return_guaranteed():
+                # Avoid generating dead instructions
+                # TODO: warn about unreachable code
+                break
 
-        # TODO: generate the 'start' and 'end' labels when required
-        #       We need to ensure each basic block has a terminating instruction
         return contained_ir
 
     def is_empty(self) -> bool:

tests/regressions/return_from_both_if_branches_unreachable.c3
@@ -1,0 +1,16 @@
+function foo : () -> void = {
+    if (true) {
+        return;
+    } else {
+        return;
+    }
+    return;
+};
+
+function main : () -> int = {
+    foo();
+    return 0;
+};
+
+/// @COMPILE
+/// @RUN