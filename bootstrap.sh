16b9c0a0f3cca42665a3caff8f37e6468aebd9eb
Antros Economides <antroseconomides@hotmail.co.uk>
2023-08-30 02:03:47 +0300

    bootstrap.sh: checkout tree in temporary directory
    Fixes #109.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,20 @@
 
 set -ex
 
-# Attempt to get the current location. Prefer a branch name if available,
-# otherwise fall back to the current commit's hash if we are in "detached HEAD"
-# state.
-git_location=$(git branch --show)
-if [[ "$git_location" == "" ]]; then
-    git_location=$(git rev-parse HEAD)
-fi
+function checkout() {
+    # Pass commit in $1 and output directory in $2.
+    mkdir -p "$2"
+    git archive --worktree-attributes "$1" | tar -x -C "$2"
+}
 
 # Stage 1; last commit where the Lark parser can parse the native parser.
-git checkout bootstrap/1
-./glang ./parser/parser.c3 -o ./parser/parser --bootstrap -O3
+stage_1_dir=./bootstrap-1
 
-# Stage 2; build native parser at current commit
-git checkout "$git_location"
+checkout bootstrap/1 $stage_1_dir
+$stage_1_dir/glang $stage_1_dir/parser/parser.c3 -o ./parser/parser --bootstrap -O3
+
+rm -rf $stage_1_dir
+
+# Stage 2; build the native parser from the working tree using the binary from
+# Stage 1.
 ./glang ./parser/parser.c3 -o ./parser/parser -O3
