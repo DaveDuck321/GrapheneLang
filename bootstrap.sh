85fae1c24de5221135f02f341568349c479f6b06
Antros Economides <antroseconomides@hotmail.co.uk>
2023-09-06 16:03:47 +0300

    Fix bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,10 @@ $stage_1_dir/glang $stage_1_dir/parser/parser.c3 -o $stage_2_dir/parser/parser -
 $stage_2_dir/glang $stage_2_dir/parser/parser.c3 -o $stage_2_dir/parser/parser
 
 # Stage 3; the parser output format has changed, compile with the latest available compiler
-$stage_2_dir/glang ./parser/parser.c3 -o ./parser/parser
+$stage_2_dir/glang ./src/glang/parser/parser.c3 -o ./src/glang/parser/parser
 
 rm -rf $stage_1_dir
 rm -rf $stage_2_dir
 
 # Final Stage; build the native parser from the working tree
-./glang ./parser/parser.c3 -o ./parser/parser -O3
+glang ./src/glang/parser/parser.c3 -o ./src/glang/parser/parser -O3
