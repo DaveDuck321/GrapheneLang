5736310cae01e84e00db7514ed9557fa6aad9c60
Antros Economides <antroseconomides@hotmail.co.uk>
2023-09-07 02:24:26 +0300

    Let hatch infer the build tag
    PyPI now only accepts manylinux-tagged builds. Let's not bother with
    that yet, so let hatch pick a tag automatically.

--- hatch_build.py
+++ hatch_build.py
@@ -6,17 +6,6 @@ from hatch.utils.platform import Platform
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
-def get_build_tag() -> str:
-    # Based on https://peps.python.org/pep-0425/. We could also use infer_tag
-    # (https://hatch.pypa.io/latest/plugins/builder/wheel/#build-data),
-    # but that is a bit too specific (we don't really about the Python ABI).
-    python_tag = "py3"  # Generic Python.
-    abi_tag = "none"  # We don't use extension modules.
-    platform_tag = get_platform().replace("-", "_").replace(".", "_")
-
-    return f"{python_tag}-{abi_tag}-{platform_tag}"
-
-
 class CustomBuildHook(BuildHookInterface):
     def clean(self, versions: list[str]):
         build_dir = Path(self.directory)
@@ -37,6 +26,6 @@ class CustomBuildHook(BuildHookInterface):
         build_data["force_include"]["dist/parser"] = "glang/bin/parser"
 
         build_data["pure_python"] = False
-        build_data["tag"] = get_build_tag()
+        build_data["infer_tag"] = True
 
         return super().initialize(version, build_data)
