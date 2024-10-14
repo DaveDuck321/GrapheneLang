0abda1b6bf906cd9ed2a5a7d3f068a389a5f531a
Antros Economides <antroseconomides@hotmail.co.uk>
2024-10-14 22:34:48 +0300

    Build gls

--- hatch_build.py
+++ hatch_build.py
@@ -6,27 +6,33 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 class CustomBuildHook(BuildHookInterface):
-    def clean(self, versions: list[str]):
+    def clean(self, versions: list[str]) -> None:
         build_dir = Path(self.directory)
-        parser_bin = build_dir / "parser"
 
-        if parser_bin.is_file():
-            parser_bin.unlink()
+        for binary in ("parser", "gls"):
+            (build_dir / binary).unlink(missing_ok=True)
 
-        return super().clean(versions)
+        super().clean(versions)
 
-    def initialize(self, version: str, build_data: dict[str, Any]):
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         self.app.display_waiting("Bootstrapping parser")
-
         rc = run(["./bootstrap.sh"], check=False).returncode
         if rc:
             self.app.abort("Bootstrap failed", rc)
 
+        self.app.display_waiting("Compiling gls")
+        rc = run(
+            ["glang", "src/lsp-server/server.c3", "-o", "dist/gls", "-O3"], check=False
+        ).returncode
+        if rc:
+            self.app.abort("gls compilation failed", rc)
+
         self.app.display_success("Done!")
 
         build_data["force_include"]["dist/parser"] = "glang/bin/parser"
+        build_data["force_include"]["dist/gls"] = "glang/bin/gls"
 
         build_data["pure_python"] = False
         build_data["infer_tag"] = True
 
-        return super().initialize(version, build_data)
+        super().initialize(version, build_data)
